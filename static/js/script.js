20f25988a4e90a1347df687a237afece5cf3b707
JimLynx <jim.lynx@gmail.com>
2021-02-14 10:59:46 +0000

    Restructure navbar with custom mobile side-menu and style reponsively.

---
+++ static/js/script.js
@@ -0,0 +1,8 @@
+// function to open and close side navigation
+function openNav() {
+    document.getElementById("mySidenav").style.width = "200px";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
