566641399662dca1957f557dc5a6a49b8850d6df
JimLynx <jim.lynx@gmail.com>
2021-02-25 15:05:26 +0000

    Add project models and load initial fixtures

---
+++ projects/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+
+class Category(models.Model):
+
+    class Meta:
+        # adjust verbose name from the Django defaults
+        verbose_name_plural = 'Categories'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """
+        string method to return category name 
+        """
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        string method to return category friendly name 
+        """
+        return self.friendly_name
+
+
+class Country(models.Model):
+    class Meta:
+        verbose_name_plural = 'Countries'
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        """
+        string method to return category name 
+        """
+        return self.name
+
+    def get_friendly_name(self):
+        """
+        string method to return category friendly name 
+        """
+        return self.friendly_name
+
+
+class Project(models.Model):
+    category = models.ForeignKey(
+        'Category', null=True, on_delete=models.SET_NULL)
+    country = models.ForeignKey(
+        'Country', null=True, on_delete=models.SET_NULL)
+    location = models.CharField(max_length=254)
+    pin_id = models.CharField(max_length=254, null=True, blank=True)
+    name = models.CharField(max_length=254)
+    lead_in = models.TextField()
+    description = models.TextField()
+    needs = models.TextField()
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        """
+        string method to return category name 
+        """
+        return self.name
