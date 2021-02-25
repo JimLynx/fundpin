a0c798d7baddb058298274aeb259fbdc2d3f27f2
JimLynx <jim.lynx@gmail.com>
2021-02-25 15:30:01 +0000

    Create projects views, urls and templates

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Project
 
 # Create your views here.
+def all_projects(request):
+    """ A view to show all projects """
+
+    projects = Project.objects.all()
+
+    context = {
+        'projects': projects,
+    }
+    return render(request, 'projects/projects.html', context)
