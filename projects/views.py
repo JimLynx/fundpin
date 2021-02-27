96f4a23f9c3f9d9955cbff24941a468e670ddce3
JimLynx <jim.lynx@gmail.com>
2021-02-27 18:49:34 +0000

    Add Country search option and link dropdown on navbar

--- projects/views.py
+++ projects/views.py
@@ -1,19 +1,25 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Project, Category
+from .models import Project, Category, Country
 
 def all_projects(request):
-    """ A view to show all projects """
+    """ A view to show all projects, including search function"""
 
     projects = Project.objects.all()
     query = None
     categories = None
+    countries = None
 
     if 'category' in request.GET:
         categories = request.GET['category'].split(',')
         projects = projects.filter(category__name__in=categories)
         categories = Category.objects.filter(name__in=categories)
+
+    if 'country' in request.GET:
+        countries = request.GET['country'].split(',')
+        projects = projects.filter(country__name__in=countries)
+        countries = Country.objects.filter(name__in=countries)
 
     if request.GET:
         if 'q' in request.GET:
@@ -32,6 +38,7 @@ def all_projects(request):
         'projects': projects,
         'search_term': query,
         'current_categories': categories,
+        'current_country': countries,
     }
     return render(request, 'projects/projects.html', context)
 
