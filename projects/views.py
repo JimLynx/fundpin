fb3031e1c1550f0c3abecf38817cea0ed50d2007
JimLynx <jim.lynx@gmail.com>
2021-02-27 16:57:38 +0000

    Edit search field in navbar, style and add functionality

--- projects/views.py
+++ projects/views.py
@@ -1,13 +1,30 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Project
 
 def all_projects(request):
     """ A view to show all projects """
 
     projects = Project.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "Please enter a search item")
+                return redirect(reverse('home'))
+            
+            queries = Q(name__icontains=query) | Q(
+                description__icontains=query)
+            projects = projects.filter(queries)
+
+
 
     context = {
         'projects': projects,
+        'search_term': query,
     }
     return render(request, 'projects/projects.html', context)
 
