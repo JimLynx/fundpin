443cb969f56b587ac3525b729733e597307cad35
JimLynx <jim.lynx@gmail.com>
2021-03-02 14:02:14 +0000

    Fix rendered total and refactor code

--- projects/static/projects/js/donationform.js
+++ projects/static/projects/js/donationform.js
@@ -1,31 +1,22 @@
-// get select input preset 
 const presetAmount = document.querySelector('#presetAmount');
-// get user-entered input 
 const userAmount = document.querySelector('#userAmount');
-// get donation total input
 const donationTotal = document.querySelector('.donationTotal');
 
-// change user entered amount to none if preset amount selected
-presetAmount.onchange = (e) => {
-    userAmount.value = "";
-
-    // get total value from preset options
-    donationTotal.value = e.target.value;
-    console.log(donationTotal.value);
-}
-
-userAmount.onkeyup = (e) => updateUserDonation(e)
-userAmount.onchange = (e) => updateUserDonation(e)
+presetAmount.onchange = (e) => updateUserDonation(e, 'preset')
+userAmount.onkeyup = (e) => updateUserDonation(e, 'user')
+userAmount.onchange = (e) => updateUserDonation(e, 'user')
 
 // change preset amount to none (0 index = no int value) if preset amount selected
-const updateUserDonation = (e) => {
-    presetAmount.selectedIndex = "0";
+const updateUserDonation = (e, option) => {
+
+    option === 'preset'
+        ? userAmount.value = ""
+        : presetAmount.selectedIndex = "0"
 
     // get total value from user entered option
     donationTotal.value = e.target.value;
     console.log(donationTotal.value)
 
-    // TBA (possible alternative to input returned total)
-    // get total and return as HTML string <= currently only returning custom value
-    // document.getElementById('displayAmount').innerHTML = donationTotal.value;
+    // get total and return as HTML string 
+    document.getElementById('displayAmount').innerHTML = donationTotal.value;
 }
