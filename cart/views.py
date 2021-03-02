c241740a5f0dcbfd3971e2be267b83a5fd6d8f68
JimLynx <jim.lynx@gmail.com>
2021-03-02 14:03:57 +0000

    Add 'add to cart' view and url

--- cart/views.py
+++ cart/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-# Create your views here.
+
 def view_cart(request):
     """ return donation cart page """
     return render(request, 'cart/cart.html')
 
+
+def add_to_cart(request, item_id):
+    """ add total donation selecrted to donation cart """
+
+    donation_amount = int(request.POST.get('donation_amount'))
+    redirect_url = request.POST.get('redirect_url')
+    cart = request.session.get('cart', {})
+
+    cart[item_id] = donation_amount
+
+    request.session['cart'] = cart
+
+    print(request.session['cart'])
+    return redirect(redirect_url)
