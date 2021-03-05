e67e7b5210c2fe07afef71e3d8d42a4f7d18b7e0
JimLynx <jim.lynx@gmail.com>
2021-03-05 12:57:21 +0000

    Update context and render basic details on donation cart page

--- cart/contexts.py
+++ cart/contexts.py
@@ -7,10 +7,20 @@ from projects.models import Project
 def cart_contents(request):
 
     cart_items = []
+    donation_amount = 0
     total = 0
     project_count = 0
 
     cart = request.session.get('cart', {})
+
+    for item_id, donation_amount in cart.items():
+        project = get_object_or_404(Project, pk=item_id)
+        total += donation_amount
+        cart_items.append({
+            'item_id': item_id,
+            'project': project,
+            'donation_amount': donation_amount
+        })
 
     context = {
         'cart_items': cart_items,
