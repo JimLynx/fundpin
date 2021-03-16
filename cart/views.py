ac9f0feec35f360d4835d60621d294956204218c
JimLynx <jim.lynx@gmail.com>
2021-03-16 14:22:53 +0000

    Create remove donation from cart view, url, functionality and initial styling

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from projects.models import Project
 
 
 def view_cart(request):
@@ -7,8 +9,9 @@ def view_cart(request):
 
 
 def add_to_cart(request, item_id):
-    """ add total donation selecrted to donation cart """
+    """ add total donation selected to donation cart """
 
+    project = get_object_or_404(Project, pk=item_id)
     donation_amount = int(request.POST.get('donation_amount'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -16,6 +19,21 @@ def add_to_cart(request, item_id):
     cart[item_id] = donation_amount
 
     request.session['cart'] = cart
-
-    print(request.session['cart'])
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, item_id):
+    """ remove item from donation cart """
+
+    try:
+        project = get_object_or_404(Project, pk=item_id)
+        cart = request.session.get('cart', {})
+
+        cart.pop(item_id)
+
+        request.session['cart'] = cart
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
+        return HttpResponse(status=500)
