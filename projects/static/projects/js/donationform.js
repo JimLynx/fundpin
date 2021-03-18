89d8f51b5bdec63e15f6d7a2018c453f55af8b55
JimLynx <jim.lynx@gmail.com>
2021-03-18 13:38:18 +0000

    Add defensive measure to prevent default & disable submit btn if no input amount selected

--- projects/static/projects/js/donationform.js
+++ projects/static/projects/js/donationform.js
@@ -1,10 +1,13 @@
+const form = document.querySelector('#DonationOptionForm');
 const presetAmount = document.querySelector('#presetAmount');
 const userAmount = document.querySelector('#userAmount');
 const donationTotal = document.querySelector('.donationTotal');
+const submitButton = document.querySelector("#donationSubmit");
 
-presetAmount.onchange = (e) => updateUserDonation(e, 'preset')
-userAmount.onkeyup = (e) => updateUserDonation(e, 'user')
-userAmount.onchange = (e) => updateUserDonation(e, 'user')
+presetAmount.onchange = (e) => updateUserDonation(e, 'preset');
+userAmount.onkeyup = (e) => updateUserDonation(e, 'user');
+userAmount.onchange = (e) => updateUserDonation(e, 'user');
+form.onsubmit = (e) => submitForm(e);
 
 // change preset amount to none (0 index = no int value) if preset amount selected
 const updateUserDonation = (e, option) => {
@@ -15,8 +18,26 @@ const updateUserDonation = (e, option) => {
 
     // get total value from user entered option
     donationTotal.value = e.target.value;
-    console.log(donationTotal.value)
+
+    // add disabled class if if total is zero
+
+    if (donationTotal.value == "0" || donationTotal.value == "") {
+        submitButton.disabled = true;
+        submitButton.style.cursor = 'not-allowed';
+    } else {
+        submitButton.disabled = false;
+        submitButton.style.cursor = 'pointer';
+    }
 
     // get total and return as HTML string 
     document.getElementById('displayAmount').innerHTML = donationTotal.value;
+}
+
+// prevent form submitting if total is zero
+const submitForm = (e) => {
+    if (donationTotal.value === "0" || donationTotal.value === "") {
+        e.preventDefault();
+    } else {
+        form.submit()
+    }
 }
