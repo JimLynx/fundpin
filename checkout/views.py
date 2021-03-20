668dd8b1b6983ab806b0ed23588e2aca1f57989e
JimLynx <jim.lynx@gmail.com>
2021-03-20 15:36:46 +0000

    Add initial Stripe elements, js & styling

--- checkout/views.py
+++ checkout/views.py
@@ -13,7 +13,9 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'stripe_public_key',
+        'stripe_secret': 'stripe_secret',
     }
 
     return render(request, template, context)
