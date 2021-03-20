9392f217b1f66bdad33373bdb4b5a196ff589908
JimLynx <jim.lynx@gmail.com>
2021-03-20 14:21:16 +0000

    Add checkout views, urls, templates & initial styling

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
-# Create your views here.
+
+def checkout(request):
+    cart = request.session.get('cart', {})
+    if not cart:
+        messages.error(
+            request, "Oops, there are no projects in your cart yet")
+        return redirect(reverse('projects'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
