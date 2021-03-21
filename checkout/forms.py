eb89eea15a52f718e750a0f4110d58d6b8a0d0da
JimLynx <jim.lynx@gmail.com>
2021-03-21 14:10:24 +0000

    Minor: change order of fields

--- checkout/forms.py
+++ checkout/forms.py
@@ -29,8 +29,8 @@ class OrderForm(forms.ModelForm):
             'phone_number': 'Phone Number',
             'street_address1': 'Street Address 1',
             'street_address2': 'Street Address 2',
-            'county': 'County, State or Locality',
             'town_or_city': 'Town or City',
+            'county': 'County, State or Locality',
             'postcode': 'Postal Code',
             'country': 'Country',
         }
