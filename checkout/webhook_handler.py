6032b722f780990ff9c696d8c4670304454227e3
JimLynx <jim.lynx@gmail.com>
2021-03-22 15:12:47 +0000

    Add save info & intent for webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -19,6 +19,75 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        pid = intent.id
+        cart = intent.metadata.cart
+        save_info = intent.metadata.save_info
+
+        billing_info = intent.charges.data[0].billing_info
+        total = round(intent.data.charges[0].amount / 100, 2)
+
+        order_exists = False
+        try:
+            order = Order.object.get(
+                full_name__iexact=billing_info.name,
+                email__iexact=billing_info.email,
+                phone_number__iexact=billing_info.phone,
+                country__iexact=billing_info.country,
+                postcode__iexact=billing_info.postal_code,
+                town_or_city__iexact=billing_info.city,
+                street_address1__iexact=billing_info.line1,
+                street_address2__iexact=billing_info.line2,
+                county__iexact=billing_info.state,
+                total__iexact=billing_info.total,
+            )
+            order_exists = True
+
+            return HttpResponse(
+                content=f'Webhook received: {event["type"]} | SUCCESS: Verified order already in database',
+                status=200)
+        except Order.DoesNotExist:
+            try:
+                order = Order.objects.create(
+                    full_name=billing_info.name,
+                    email=billing_info.email,
+                    phone_number=billing_info.phone,
+                    country=billing_info.country,
+                    postcode=billing_info.postal_code,
+                    town_or_city=billing_info.city,
+                    street_address1=billing_info.line1,
+                    street_address2=billing_info.line2,
+                    county=billing_info.state,
+                )
+                for item_id, item_data in json.loads(cart).items():
+                    project = Project.objects.get(id=item_id)
+                    donation_amount = Decimal(item_data)
+
+                    order_line_item = OrderLineItem(
+                        order=order,
+                        project=project,
+                        lineitem_total=donation_amount,
+                    )
+                    order_line_item.save()
+                    order_line_item.order.total += order_line_item.lineitem_total
+                    order_line_item.save()
+            except Exception as e:
+                if order:
+                    order.delete()
+                return HttpResponse(
+                    content=f'Webhook received: {event["type"]} | ERROR: {e}',
+                    status=500)
+
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.failed webhook from Stripe
+        """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]} | SUCCESS: Created order in webhook',
             status=200)
