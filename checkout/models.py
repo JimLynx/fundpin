a5149fc0536e367b80f5d5403279c8fd1775ff0e
JimLynx <jim.lynx@gmail.com>
2021-04-01 10:52:07 +0100

    Minor: redorder db list to correspond with site display order for consistency

--- checkout/models.py
+++ checkout/models.py
@@ -16,8 +16,8 @@ class Order(models.Model):
 
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
-    county = models.CharField(max_length=80, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
+    county = models.CharField(max_length=80, null=True, blank=True)
     postcode = models.CharField(max_length=20, null=True, blank=True)
     country = CountryField(blank_label='Country *', null=False, blank=False)
 
