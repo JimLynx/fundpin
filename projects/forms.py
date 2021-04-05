7faa54132f9ec1536d90b224459c068366a3d58e
JimLynx <jim.lynx@gmail.com>
2021-04-05 14:49:43 +0100

    Add custom clearable input for image uploads

--- projects/forms.py
+++ projects/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Project, Category, Country
 
 
@@ -8,12 +9,16 @@ class ProjectForm(forms.ModelForm):
         model = Project
         fields = '__all__'
 
+    image = forms.ImageField(label='Image', required=False, widget=CustomClearableFileInput)
+
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         countries = Country.objects.all()
-        cat_friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
-        country_friendly_names = [(country.id, country.get_friendly_name()) for country in countries]
+        cat_friendly_names = [(c.id, c.get_friendly_name())
+                              for c in categories]
+        country_friendly_names = [
+            (country.id, country.get_friendly_name()) for country in countries]
 
         self.fields['category'].choices = cat_friendly_names
         for field_name, field in self.fields.items():
