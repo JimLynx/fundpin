95a368b028c82eeebfcaa9e1daedc9be18032670
JimLynx <jim.lynx@gmail.com>
2021-04-12 15:00:37 +0100

    Add blog description view, url, template and link css

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,7 @@
-from .views import blog_list
+from .views import blog_list, blog_description
 from django.urls import path
 
 urlpatterns = [
     path('', blog_list),
+    path('<id>/', blog_description),
 ]
--- blog/views.py
+++ blog/views.py
@@ -8,3 +8,11 @@ def blog_list(request):
         'blog_list': post
     }
     return render(request, "blog/blog_list.html", context)
+
+
+def blog_description(request, id):
+    per_post = Post.objects.get(id=id)
+    context = {
+        'blog_description': per_post
+    }
+    return render(request, "blog/blog_description.html", context)
