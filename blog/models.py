65720b720dc5b8b67df5382f4049c67227f99f57
JimLynx <jim.lynx@gmail.com>
2021-04-13 08:15:49 +0100

    Update blog model slug field to correspond with creation date

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.fields import DateTimeField
 from richtext.fields import RichTextField
 
 
@@ -10,9 +11,9 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique=True)
+    slug = models.SlugField(max_length=200, unique_for_date='updated_on')
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
-    updated_on = models.DateTimeField(auto_now= True)
+    updated_on = models.DateTimeField(auto_now=True)
     content = RichTextField(blank=False, null=False)
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
