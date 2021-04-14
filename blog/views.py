df67556f8913d79dfd9439653e6b18a6edf485eb
JimLynx <jim.lynx@gmail.com>
2021-04-14 10:06:30 +0100

    Minor update: create variable for blog templates returns

--- blog/views.py
+++ blog/views.py
@@ -1,18 +1,27 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Post
+from blog.models import Post
 
 
 def blog_list(request):
+    """ return blog list page """
+
     blog_list = Post.objects.all()
+
+    template = 'blog/blog_list.html'
     context = {
         'blogs': blog_list,
     }
-    return render(request, "blog/blog_list.html", context)
+
+    return render(request, template, context)
 
 
 def blog_description(request, blog_id):
+    """ return index pageblog description page for each blog """
+
     blog_description = get_object_or_404(Post, pk=blog_id)
+
+    template = 'blog/blog_description.html'
     context = {
         'blog_description': blog_description
     }
-    return render(request, "blog/blog_description.html", context)
+    return render(request, template, context)
