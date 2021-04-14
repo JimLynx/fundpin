4bb482a542c173fb5462850e6d90074c41ad3e33
JimLynx <jim.lynx@gmail.com>
2021-04-14 10:13:19 +0100

    Update home view to render blogs context on home page

--- home/views.py
+++ home/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from projects.models import Project
+from blog.models import Post
 
 
 def index(request):
     """ return index page """
 
     projects = Project.objects.all()
+    blog = Post.objects.all()
 
     context = {
         'projects': projects,
+        'blogs': blog
     }
 
     return render(request, 'home/index.html', context)
@@ -16,4 +19,5 @@ def index(request):
 
 def contact(request):
     """ return contact page """
+
     return render(request, 'home/contact.html')
