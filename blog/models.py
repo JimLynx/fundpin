2d67af268ebbb3c703d7e19af9e23b81d053e132
JimLynx <jim.lynx@gmail.com>
2021-04-14 12:50:42 +0100

    Minor edit on slugfield attribute

--- blog/models.py
+++ blog/models.py
@@ -11,8 +11,8 @@ STATUS = (
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
-    slug = models.SlugField(max_length=200, unique_for_date='updated_on')
-    author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
+    slug = models.SlugField(max_length=200, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now=True)
     content = RichTextField(blank=False, null=False)
     created_on = models.DateTimeField(auto_now_add=True)
