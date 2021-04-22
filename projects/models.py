1244f07ac8fa50d00c238827afe2742062986e48
JimLynx <jim.lynx@gmail.com>
2021-04-22 08:47:07 +0100

    Update pin_id in project model to be a unique and required field

--- projects/models.py
+++ projects/models.py
@@ -45,12 +45,12 @@ class Country(models.Model):
 
 
 class Project(models.Model):
+    pin_id = models.CharField(max_length=20, null=True, blank=False, unique=True)
     category = models.ForeignKey(
         'Category', null=True, on_delete=models.SET_NULL)
     country = models.ForeignKey(
         'Country', null=True, on_delete=models.SET_NULL)
     location = models.CharField(max_length=254)
-    pin_id = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = RichTextField(blank=False, null=True)
     needs = RichTextField(blank=False, null=True)
